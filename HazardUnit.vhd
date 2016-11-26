ef98c00f5d1bbc3a84526602cba9991e91c08448
Harshvardhan1 <hrtibrewal@gmail.com>
2016-11-26 23:43:02 +0530

    minor changes

--- HazardUnit.vhd
+++ HazardUnit.vhd
@@ -27,18 +27,18 @@ carry_ex: in std_logic;
 zero_ex: in std_logic;
 regDest: in matrix3(0 to 4);               --0- for IF, 1-ID, 2-RR, 3-EX, 4-MEM
 Lm_sel: in std_logic_vector(7 downto 0);
-Instr_out: out matrix16(0 to 5);
+Instr_out: out matrix16(0 to 4);
 pipeline_enable: out std_logic_vector(0 to 4)  --  0- for IF, 1-ID, 2-RR, 3-EX, 4-MEM
          ) ;
 end HazardUnit ;
 -----
 architecture behave of HazardUnit is
 signal Stalled_flushed_instr: matrix16(0 to 4);
-signal pipeline_reg_enable : std_logic_vector(0 to 5);
+signal pipeline_reg_enable : std_logic_vector(0 to 4);
 begin
   process( Instruction_pipeline, Lm_sel, carry_ex, zero_ex, stallflag, regDest )
     variable var_instr_out: matrix16(0 to 4) := Instruction_pipeline;
-    variable var_pipeline_reg_enable : std_logic_vector(0 to 5):="111111";
+    variable var_pipeline_reg_enable : std_logic_vector(0 to 4):="11111";
     begin
       ------
       if (stallflag='1') then
@@ -46,13 +46,13 @@ begin
           var_pipeline_reg_enable(2) := '0';            -- stall the RR stage
       end if;
       ------
-      if ( (regDest(4)="111" and Instruction_pipeline(4)(15 downto 12)="0100") or (Lm_sel(7)=='1' and Instruction_pipeline(4)(15 downto 12)="0110" ) ) then    -- check for hazard in mem stage LOAD instr
+      if ( (regDest(4)="111" and Instruction_pipeline(4)(15 downto 12)="0100") or (Lm_sel(7)='1' and Instruction_pipeline(4)(15 downto 12)="0110" ) ) then    -- check for hazard in mem stage LOAD instr
           -- flush 4 stages
           var_instr_out(3)(15 downto 12) := "1111";
           var_instr_out(2)(15 downto 12) := "1111";
           var_instr_out(1)(15 downto 12) := "1111";
           var_instr_out(0)(15 downto 12) := "1111";
-      elsif ( regDest(3)="111" and Instruction_pipeline(3)(15 downto 14)="00" and Instruction_pipeline(3)(15 downto 14) /= "11"  )----  R type
+      elsif ( regDest(3)="111" and Instruction_pipeline(3)(15 downto 14)="00" and Instruction_pipeline(3)(15 downto 14) /= "11"  ) then----  R type
           if (( Instruction_pipeline(3)(13)='1' and carry_ex='1') or ( Instruction_pipeline(3)(12)='1' and zero_ex='1') or (Instruction_pipeline(3)(13 downto 12)="00")) then
               var_instr_out(2)(15 downto 12) := "1111";
               var_instr_out(1)(15 downto 12) := "1111";
