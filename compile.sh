313e42b9e89f9319ecc5b343d30ceb01e4941b8a
Amritesh Sharma <amritesh.iitb@gmail.com>
2016-11-28 23:56:30 +0530

    compile file

--- compile.sh
+++ compile.sh
@@ -12,3 +12,7 @@ ghdl -a PipelineRegister.vhd
 ghdl -a regfile.vhd
 ghdl -a HazardUnit.vhd
 ghdl -a Datapath.vhd
+ghdl -a pilot.vhd
+ghdl -a final_test.vhd
+ghdl -m Grand_Testh
+./grand_testh --wave=run.ghw --stop-time=5000ns
