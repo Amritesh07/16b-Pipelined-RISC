0fa0908130b78390170379beebfbb2a8dd9cc55e
Amritesh Sharma <amritesh.iitb@gmail.com>
2016-11-28 06:24:15 +0530

    ALU corrected

--- ALU.vhd
+++ ALU.vhd
@@ -11,13 +11,13 @@ port(
 end ALU;
 
 architecture arch of ALU is
-	signal: add_sig,nand_sig std_logic_vector(16 downto 0);
-	signal: output_sig std_logic_vector(15 downto 0)
+	signal add_sig,nand_sig: std_logic_vector(16 downto 0);
+	signal output_sig: std_logic_vector(15 downto 0);
 begin
 	add_sig<=std_logic_vector(unsigned(I1)+unsigned(I2));
 	nand_sig<=I1 nand I2;
-	output_sig<= (sel and (nand_sig(15  downto 0)))or(not(sel)and(add_sig(15 downto 0));
-	O<=output_sig
-	C<= not(sel)and(add_sig(16);		-- 16th bit of addition is carry bit
+	output_sig <= nand_sig(15  downto 0) when sel ='1' else add_sig(15 downto 0);
+	O<=output_sig;
+	C<= not(sel) and add_sig(16);		-- 16th bit of addition is carry bit
 	Z<= '1' when output_sig="0000000000000000" else '0';
 end arch;
